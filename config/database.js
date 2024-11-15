aa7ba8daa9366e6cc3ab907af7fa94604537a1dd
Ujjwal Sapkota <bisiness.ujjwal@gmail.com>
2024-11-15 20:40:41 +0545

    dsajb

--- config/database.js
+++ config/database.js
@@ -1,46 +1,38 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-// const connectDb = async (dburi) =>{
-// try{
-//     await mongoose.connect(`${dburi}/scholibNew`,{
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//     })
-//     console.log('mongodb connected successfully')
-
-// }
-// catch (error){
-// console.log(error)
-// console.log('mongodb failed to connect')
-// }
-// }
-
-// module.exports = connectDb;
-
-
-
-
-
-
-// This one is for the server so make sure to make it the way it works
-
-const connectDb = async (dburi = "mongodb://127.0.0.1:27017", username='adminUsername', password='password123') => {
-    try {
-        // Construct the full MongoDB URI with username and password
-        const uri = `${dburi}/scholibNew?authSource=admin`;
-        
-        await mongoose.connect(uri, {
-            user: username,
-            pass: password,
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected successfully');
-    } catch (error) {
-        console.log(error);
-        console.log('MongoDB failed to connect');
-    }
+const connectDb = async (dburi) => {
+  try {
+    await mongoose.connect(`${dburi}/scholibNew`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("mongodb connected successfully");
+  } catch (error) {
+    console.log(error);
+    console.log("mongodb failed to connect");
+  }
 };
 
 module.exports = connectDb;
 
+// This one is for the server so make sure to make it the way it works
+
+// const connectDb = async (dburi = "mongodb://127.0.0.1:27017", username='adminUsername', password='password123') => {
+//     try {
+//         // Construct the full MongoDB URI with username and password
+//         const uri = `${dburi}/scholibNew?authSource=admin`;
+
+//         await mongoose.connect(uri, {
+//             user: username,
+//             pass: password,
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log('MongoDB connected successfully');
+//     } catch (error) {
+//         console.log(error);
+//         console.log('MongoDB failed to connect');
+//     }
+// };
+
+// module.exports = connectDb;
